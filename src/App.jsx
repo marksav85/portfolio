d63d54cd1157300bb190ca847de2343798e4a683
Mark Saville <mark.saville@outlook.com>
2023-10-27 15:30:52 +0200

    add all component views

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import { useState } from "react";
 import React from "react";
 import { Header } from "./components/header-section/header-section";
+import { Profile } from "./components/profile-section/profile-section";
+import { Work } from "./components/work-section/work-section";
+import { Contact } from "./components/contact-section/contact-section";
 
 function App() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <Profile />
+      <Work />
+      <Contact />
+    </div>
+  );
 }
 
 export default App;
