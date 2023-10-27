d150405a187d5ffb8f95457a6b3352d8569a452e
Mark Saville <mark.saville@outlook.com>
2023-10-27 13:23:18 +0200

    create work-section component

---
+++ src/components/work-section/work-section.jsx
@@ -0,0 +1,80 @@
+export const Work = () => {
+  return (
+    <>
+      <section id="two">
+        <h2>Recent Work</h2>
+        <div className="row">
+          <article className="col-6 col-12-xsmall work-item">
+            <a
+              className="image fit thumb"
+              style="cursor: pointer; outline: 0px;"
+            >
+              <img src="" alt=""></img>
+            </a>
+            <h3>Magna sed consequat tempus</h3>
+            <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
+          </article>
+          <article className="col-6 col-12-xsmall work-item">
+            <a
+              className="image fit thumb"
+              style="cursor: pointer; outline: 0px;"
+            >
+              <img src="" alt=""></img>
+            </a>
+            <h3>Ultricies lacinia interdum</h3>
+            <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
+          </article>
+          <article className="col-6 col-12-xsmall work-item">
+            <a
+              className="image fit thumb"
+              style="cursor: pointer; outline: 0px;"
+            >
+              <img src="" alt=""></img>
+            </a>
+            <h3>Tortor metus commodo</h3>
+            <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
+          </article>
+          <article className="col-6 col-12-xsmall work-item">
+            <a
+              className="image fit thumb"
+              style="cursor: pointer; outline: 0px;"
+            >
+              <img src="" alt=""></img>
+            </a>
+            <h3>Quam neque phasellus</h3>
+            <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
+          </article>
+          <article className="col-6 col-12-xsmall work-item">
+            <a
+              className="image fit thumb"
+              style="cursor: pointer; outline: 0px;"
+            >
+              <img src="" alt=""></img>
+            </a>
+            <h3>Nunc enim commodo aliquet</h3>
+            <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
+          </article>
+          <article className="col-6 col-12-xsmall work-item">
+            <a
+              className="image fit thumb"
+              style="cursor: pointer; outline: 0px;"
+            >
+              <img src="" alt=""></img>
+            </a>
+            <h3>Risus ornare lacinia</h3>
+            <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
+          </article>
+        </div>
+        <ul className="actions">
+          <li>
+            <a href="#" className="button">
+              Full Portfolio
+            </a>
+          </li>
+        </ul>
+      </section>
+    </>
+  );
+};
+
+export default Work;
