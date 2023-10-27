3d82d0e01e7103bb35e306b2c4819731af7166de
Mark Saville <mark.saville@outlook.com>
2023-10-27 15:29:14 +0200

    remove inline styling

--- src/components/work-section/work-section.jsx
+++ src/components/work-section/work-section.jsx
@@ -5,60 +5,42 @@ export const Work = () => {
         <h2>Recent Work</h2>
         <div className="row">
           <article className="col-6 col-12-xsmall work-item">
-            <a
-              className="image fit thumb"
-              style="cursor: pointer; outline: 0px;"
-            >
+            <a className="image fit thumb">
               <img src="" alt=""></img>
             </a>
             <h3>Magna sed consequat tempus</h3>
             <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
           </article>
           <article className="col-6 col-12-xsmall work-item">
-            <a
-              className="image fit thumb"
-              style="cursor: pointer; outline: 0px;"
-            >
+            <a className="image fit thumb">
               <img src="" alt=""></img>
             </a>
             <h3>Ultricies lacinia interdum</h3>
             <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
           </article>
           <article className="col-6 col-12-xsmall work-item">
-            <a
-              className="image fit thumb"
-              style="cursor: pointer; outline: 0px;"
-            >
+            <a className="image fit thumb">
               <img src="" alt=""></img>
             </a>
             <h3>Tortor metus commodo</h3>
             <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
           </article>
           <article className="col-6 col-12-xsmall work-item">
-            <a
-              className="image fit thumb"
-              style="cursor: pointer; outline: 0px;"
-            >
+            <a className="image fit thumb">
               <img src="" alt=""></img>
             </a>
             <h3>Quam neque phasellus</h3>
             <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
           </article>
           <article className="col-6 col-12-xsmall work-item">
-            <a
-              className="image fit thumb"
-              style="cursor: pointer; outline: 0px;"
-            >
+            <a className="image fit thumb">
               <img src="" alt=""></img>
             </a>
             <h3>Nunc enim commodo aliquet</h3>
             <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
           </article>
           <article className="col-6 col-12-xsmall work-item">
-            <a
-              className="image fit thumb"
-              style="cursor: pointer; outline: 0px;"
-            >
+            <a className="image fit thumb">
               <img src="" alt=""></img>
             </a>
             <h3>Risus ornare lacinia</h3>
@@ -76,5 +58,3 @@ export const Work = () => {
     </>
   );
 };
-
-export default Work;
