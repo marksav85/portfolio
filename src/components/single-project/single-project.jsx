c46c1c4497d54667e4e75d43c000ca9192f50496
Mark Saville <mark.saville@outlook.com>
2023-11-07 17:22:56 +0100

    create single project component

---
+++ src/components/single-project/single-project.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export function SingleProject({ title, description, image }) {
+  const imageStyle = {
+    cursor: "pointer",
+    outline: "0px",
+  };
+  return (
+    <article className="col-6 col-12-xsmall work-item">
+      <a href={image} className="image fit thumb" style={imageStyle}>
+        <img src={image} alt={title}></img>
+      </a>
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </article>
+  );
+}
+
+SingleProject.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+};
