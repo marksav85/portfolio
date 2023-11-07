8fd460e051f7a4da2843ce60f7f8d70f149c8939
Mark Saville <mark.saville@outlook.com>
2023-11-07 13:57:58 +0100

    move images to public folder

--- src/components/work-section/work-section.jsx
+++ src/components/work-section/work-section.jsx
@@ -1,47 +1,51 @@
 export const Work = () => {
+  const imageStyle = {
+    cursor: "pointer",
+    outline: "0px",
+  };
   return (
     <>
       <section id="two">
         <h2>Recent Work</h2>
         <div className="row">
           <article className="col-6 col-12-xsmall work-item">
-            <a className="image fit thumb">
-              <img src="" alt=""></img>
+            <a className="image fit thumb" style={imageStyle}>
+              <img src="/images/thumbs/01.jpg" alt="project one image"></img>
             </a>
             <h3>Magna sed consequat tempus</h3>
             <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
           </article>
           <article className="col-6 col-12-xsmall work-item">
-            <a className="image fit thumb">
-              <img src="" alt=""></img>
+            <a className="image fit thumb" style={imageStyle}>
+              <img src="/images/thumbs/02.jpg" alt="project two image"></img>
             </a>
             <h3>Ultricies lacinia interdum</h3>
             <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
           </article>
           <article className="col-6 col-12-xsmall work-item">
-            <a className="image fit thumb">
-              <img src="" alt=""></img>
+            <a className="image fit thumb" style={imageStyle}>
+              <img src="/images/thumbs/03.jpg" alt="project three image"></img>
             </a>
             <h3>Tortor metus commodo</h3>
             <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
           </article>
           <article className="col-6 col-12-xsmall work-item">
-            <a className="image fit thumb">
-              <img src="" alt=""></img>
+            <a className="image fit thumb" style={imageStyle}>
+              <img src="/images/thumbs/04.jpg" alt="project four image"></img>
             </a>
             <h3>Quam neque phasellus</h3>
             <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
           </article>
           <article className="col-6 col-12-xsmall work-item">
-            <a className="image fit thumb">
-              <img src="" alt=""></img>
+            <a className="image fit thumb" style={imageStyle}>
+              <img src="/images/thumbs/05.jpg" alt="project five image"></img>
             </a>
             <h3>Nunc enim commodo aliquet</h3>
             <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
           </article>
           <article className="col-6 col-12-xsmall work-item">
-            <a className="image fit thumb">
-              <img src="" alt=""></img>
+            <a className="image fit thumb" style={imageStyle}>
+              <img src="/images/thumbs/06.jpg" alt="project six image"></img>
             </a>
             <h3>Risus ornare lacinia</h3>
             <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
