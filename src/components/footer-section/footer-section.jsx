c1b3566615dede09c57299afa9cd8e41557aeb73
Mark Saville <mark.saville@outlook.com>
2023-11-08 19:15:07 +0100

    update footer links and visibility

--- src/components/footer-section/footer-section.jsx
+++ src/components/footer-section/footer-section.jsx
@@ -3,26 +3,38 @@ import React from "react";
 export const Footer = () => {
   return (
     <div>
-      <footer id="footer">
+      <footer id="footer" className="footer-bottom">
         <div className="inner">
           <ul className="icons">
             <li>
-              <a href="#" className="icon brands fa-twitter">
+              <a
+                href="https://twitter.com/marksav85"
+                className="icon brands fa-twitter"
+              >
                 <span className="label">Twitter</span>
               </a>
             </li>
             <li>
-              <a href="#" className="icon brands fa-github">
+              <a
+                href="https://github.com/marksav85"
+                className="icon brands fa-github"
+              >
                 <span className="label">Github</span>
               </a>
             </li>
             <li>
-              <a href="#" className="icon brands fa-linkedin">
+              <a
+                href="https://linkedin.com/in/mark-saville"
+                className="icon brands fa-linkedin"
+              >
                 <span className="label">Linkedin</span>
               </a>
             </li>
             <li>
-              <a href="#" className="icon solid fa-envelope">
+              <a
+                href="mailto:marksav85@gmail.com?subject=Test%20test&body=This%20is%20a%20test"
+                className="icon solid fa-envelope"
+              >
                 <span className="label">Email</span>
               </a>
             </li>
