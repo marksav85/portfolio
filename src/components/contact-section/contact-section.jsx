ccd13b660f6cee9d170412d9f68f07feafafc04b
Mark Saville <mark.saville@outlook.com>
2023-11-20 12:05:07 +0100

    update paths for contact

--- src/components/contact-section/contact-section.jsx
+++ src/components/contact-section/contact-section.jsx
@@ -36,13 +36,10 @@ export const Contact = ({ isEnglish }) => {
                 </a>
               </li>
               <li>
-                <h3
-                  className="icon solid fa-download"
-                  href="./documents/Saville-Mark-Resume.docx"
-                >
+                <h3 className="icon solid fa-download">
                   <span className="label">Download</span>
                 </h3>
-                <a href="/documents/Saville-Mark-Resume.docx">Download my CV</a>
+                <a href="documents/Saville-Mark-Resume.docx">Download my CV</a>
               </li>
             </ul>
           </div>
