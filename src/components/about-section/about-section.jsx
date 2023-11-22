f172f52113539aa9e1f9f3cef7e0f2a3d97d9a3f
Mark Saville <mark.saville@outlook.com>
2023-11-22 12:17:41 +0100

    restructure about me icon image  folder

--- src/components/about-section/about-section.jsx
+++ src/components/about-section/about-section.jsx
@@ -4,17 +4,17 @@ import * as german from "../../languages/de.json";
 
 export const About = ({ isEnglish }) => {
   const skillsData = [
-    { name: "HTML5", image: "images/icons/html5.png" },
-    { name: "CSS", image: "images/icons/css.png" },
-    { name: "JavaScript", image: "images/icons/javascript4.png" },
-    { name: "React", image: "images/icons/react.png" },
-    { name: "Angular", image: "images/icons/angular.png" },
-    { name: "TypeScript", image: "images/icons/typescript.png" },
-    { name: "Node.js", image: "images/icons/node.png" },
-    { name: "MongoDB", image: "images/icons/mongodb.png" },
-    { name: "MySQL", image: "images/icons/mysql.png" },
-    { name: "Firebase", image: "images/icons/firebase.png" },
-    { name: "AWS", image: "images/icons/aws.png" },
+    { name: "HTML5", image: "images/aboutme-icons/html5.png" },
+    { name: "CSS", image: "images/aboutme-icons/css.png" },
+    { name: "JavaScript", image: "images/aboutme-icons/javascript4.png" },
+    { name: "React", image: "images/aboutme-icons/react.png" },
+    { name: "Angular", image: "images/aboutme-icons/angular.png" },
+    { name: "TypeScript", image: "images/aboutme-icons/typescript.png" },
+    { name: "Node.js", image: "images/aboutme-icons/node.png" },
+    { name: "MongoDB", image: "images/aboutme-icons/mongodb.png" },
+    { name: "MySQL", image: "images/aboutme-icons/mysql.png" },
+    { name: "Firebase", image: "images/aboutme-icons/firebase.png" },
+    { name: "AWS", image: "images/aboutme-icons/aws.png" },
   ];
 
   const lang = isEnglish ? english : german;
@@ -25,8 +25,8 @@ export const About = ({ isEnglish }) => {
         <table className="skills-table">
           <thead>
             <tr>
-              {/*               <th></th>
-              <th></th> */}
+              <th>Technology</th>
+              <th>Icon</th>
             </tr>
           </thead>
           <tbody>
