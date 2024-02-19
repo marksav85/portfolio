b5a69f44201dbca0775886f0fa3f0b5d06521086
Mark Saville <mark.saville@outlook.com>
2024-02-19 17:42:56 +0100

    Add References section to MainView component

--- src/components/main-section/main-view.jsx
+++ src/components/main-section/main-view.jsx
@@ -1,7 +1,7 @@
-import React from "react";
 import { Profile } from "./profile-section/profile-section";
 import { Work } from "./work-section/work-section";
 import { Contact } from "./contact-section/contact-section";
+import References from "./references-section/References";
 
 export const MainView = ({ isEnglish }) => {
   return (
@@ -9,6 +9,7 @@ export const MainView = ({ isEnglish }) => {
       <Profile isEnglish={isEnglish} />
       <Work isEnglish={isEnglish} />
       <Contact isEnglish={isEnglish} />
+      <References isEnglish={isEnglish} />
     </div>
   );
 };
