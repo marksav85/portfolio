4f3169d72c6565049d4f891fc04f6d860d857992
Mark Saville <mark.saville@outlook.com>
2024-02-29 16:21:18 +0100

    Update AdditionalProjects.jsx with new project data

--- src/components/main-section/work-section/additional-projects/AdditionalProjects.jsx
+++ src/components/main-section/work-section/additional-projects/AdditionalProjects.jsx
@@ -8,24 +8,24 @@ export default function AdditionalProjects({ isEnglish }) {
 
   const workArray = [
     {
-      title: "title",
-      image1: "image1",
-      image2: "image2",
-      image3: "image3",
+      title: "My Dojo",
+      image1: "project7/image1.jpg",
+      image2: "project7/image2.jpg",
+      image3: "project7/image3.jpg",
       projectData: {
-        introText: "introText",
-        para1: "para1",
-        para2: "para2",
-        para3: "para3",
-        para4: "para4",
-        technologies: "technologies",
+        introText: `${lang.project1.introText}`,
+        para1: `${lang.project1.para1}`,
+        para2: `${lang.project1.para2}`,
+        para3: `${lang.project1.para3}`,
+        para4: `${lang.project1.para4}`,
+        technologies: `${lang.project1.technologies}`,
       },
       projectLabels: {
-        introTitle: "introintroTitleText",
-        processTitle: "processTitle",
-        technologies: "technologies",
-        site: "site",
-        repo: "repo",
+        introTitle: `${lang.projectLabels.introTitle}`,
+        processTitle: `${lang.projectLabels.processTitle}`,
+        technologies: `${lang.projectLabels.technologies}`,
+        site: `${lang.projectLabels.visitSite}`,
+        repo: `${lang.projectLabels.visitRepo}`,
       },
       links: {
         site: "https://marksav85.github.io/pokedex-app/",
@@ -33,28 +33,30 @@ export default function AdditionalProjects({ isEnglish }) {
       },
     },
     {
-      title: "title",
-      image1: "image1",
-      image2: "image2",
-      image3: "image3",
+      title: "Travel App",
+      image1: "project8/image1.jpg",
+      image2: "project8/image2.jpg",
+      image3: "project8/image3.jpg",
       projectData: {
-        introText: "introText",
-        para1: "para1",
-        para2: "para2",
-        para3: "para3",
-        para4: "para4",
-        technologies: "technologies",
+        introText: `${lang.project2.introText}`,
+        para1: `${lang.project2.para1}`,
+        para2: `${lang.project2.para2}`,
+        para3: `${lang.project2.para3}`,
+        para4: `${lang.project2.para4}`,
+        technologies: `${lang.project2.technologies}`,
+        site: `${lang.project2.visitSite}`,
+        repo: `${lang.project2.visitRepo}`,
       },
       projectLabels: {
-        introTitle: "introintroTitleText",
-        processTitle: "processTitle",
-        technologies: "technologies",
-        site: "site",
-        repo: "repo",
+        introTitle: `${lang.projectLabels.introTitle}`,
+        processTitle: `${lang.projectLabels.processTitle}`,
+        technologies: `${lang.projectLabels.technologies}`,
+        site: `${lang.projectLabels.visitSite}`,
+        repo: `${lang.projectLabels.visitRepo}`,
       },
       links: {
-        site: "https://marksav85.github.io/pokedex-app/",
-        repo: "https://github.com/marksav85/pokedex-app",
+        site: "https://github.com/marksav85/movie-api",
+        repo: "https://github.com/marksav85/movie-api",
       },
     },
   ];
@@ -62,6 +64,7 @@ export default function AdditionalProjects({ isEnglish }) {
   return (
     <>
       <section id="two">
+        <h2>{lang.workTitle.title}</h2>
         <div className="row">
           {workArray.map((workItem, index) => (
             <SingleProject
