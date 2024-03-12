8c36c85103c8717004dc89742c82838a5e548a86
Mark Saville <mark.saville@outlook.com>
2024-03-12 13:58:22 +0100

    Move pokemon app to AdditionalProjects.jsx

--- src/components/main-section/work-section/additional-projects/AdditionalProjects.jsx
+++ src/components/main-section/work-section/additional-projects/AdditionalProjects.jsx
@@ -7,7 +7,7 @@ export default function AdditionalProjects({ isEnglish }) {
 
   const workArray = [
     {
-      title: "My Dojo",
+      title: "Pokemon App",
       image1: "project7/image1.jpg",
       image2: "project7/image2.jpg",
       image3: "project7/image3.jpg",
@@ -21,16 +21,17 @@ export default function AdditionalProjects({ isEnglish }) {
       },
       projectLabels: {
         introTitle: `${lang.projectLabels.introTitle}`,
-        descTitle: `${lang.projectLabels.descTitle}`,
+        processTitle: `${lang.projectLabels.processTitle}`,
         technologies: `${lang.projectLabels.technologies}`,
         site: `${lang.projectLabels.visitSite}`,
         repo: `${lang.projectLabels.visitRepo}`,
       },
       links: {
-        site: "https://my-dojo-fc275.web.app/",
-        repo: "https://github.com/marksav85/my-dojo",
+        site: "https://marksav85.github.io/pokedex-app/",
+        repo: "https://github.com/marksav85/pokedex-app",
       },
     },
+
     {
       title: "Travel App",
       image1: "project8/image1.jpg",
@@ -56,31 +57,6 @@ export default function AdditionalProjects({ isEnglish }) {
       links: {
         site: "https://my-travel-weather-app-997ff38afb18.herokuapp.com/",
         repo: "https://github.com/marksav85/travel_app",
-      },
-    },
-    {
-      title: "Pokemon App",
-      image1: "project9/image1.jpg",
-      image2: "project9/image2.jpg",
-      image3: "project9/image3.jpg",
-      projectData: {
-        introText: `${lang.project9.introText}`,
-        para1: `${lang.project9.para1}`,
-        para2: `${lang.project9.para2}`,
-        para3: `${lang.project9.para3}`,
-        para4: `${lang.project9.para4}`,
-        technologies: `${lang.project9.technologies}`,
-      },
-      projectLabels: {
-        introTitle: `${lang.projectLabels.introTitle}`,
-        processTitle: `${lang.projectLabels.processTitle}`,
-        technologies: `${lang.projectLabels.technologies}`,
-        site: `${lang.projectLabels.visitSite}`,
-        repo: `${lang.projectLabels.visitRepo}`,
-      },
-      links: {
-        site: "https://marksav85.github.io/pokedex-app/",
-        repo: "https://github.com/marksav85/pokedex-app",
       },
     },
   ];
