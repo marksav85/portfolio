1016899d3af144c83410630e5089acc63c96678e
Mark Saville <mark.saville@outlook.com>
2024-04-08 12:21:45 +0200

    Update main.jsx to include Apollo Client and LanguageProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { LanguageProvider } from "./context/LanguageContext";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import "./assets/css/main.css";
 
+// apollo client
+const client = new ApolloClient({
+  uri: "http://localhost:1337/graphql",
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <LanguageProvider>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </LanguageProvider>
 );
