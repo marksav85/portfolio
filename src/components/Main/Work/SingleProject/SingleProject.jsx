88d55eeb3038b36773676efc5253a72ed2fe52c9
Mark Saville <mark.saville@outlook.com>
2024-04-09 19:30:08 +0200

    Refactor SingleProject component to use gql data and custom hooks

--- src/components/Main/Work/SingleProject/SingleProject.jsx
+++ src/components/Main/Work/SingleProject/SingleProject.jsx
@@ -1,35 +1,34 @@
 /* eslint-disable react/prop-types */
-import React from "react";
+import { useState } from "react";
 import { ImageModal } from "../ImageModal/ImageModal";
 import "bootstrap/dist/css/bootstrap.min.css";
-import * as english from "../../../../languages/en.json";
-import * as german from "../../../../languages/de.json";
+// Import language-related context and custom hook
+import { useLanguage } from "../../../../context/LanguageContext";
+import useLanguageContent from "../../../../hooks/useLanguageContent";
 
-export function SingleProject({
-  title,
-  image1,
-  image2,
-  image3,
-  projectData,
-  projectLabels,
-  links,
-  isEnglish,
-}) {
-  const lang = isEnglish ? english : german;
+export function SingleProject({ projectNumber }) {
+  // Use language-related context and custom hook
+  const { handleLanguageChange } = useLanguage();
+  const language = useLanguageContent();
+
   const imageStyle = {
     cursor: "pointer",
     outline: "0px",
   };
-  const [isVisible, setIsVisible] = React.useState(false);
+  const [isVisible, setIsVisible] = useState(false);
   const toggleVisibility = () => {
     setIsVisible(!isVisible);
   };
 
-  const [modalShow, setModalShow] = React.useState(false);
+  const [modalShow, setModalShow] = useState(false);
 
-  const firstImage = `images/project-images/${image1}`;
-  const secondImage = `images/project-images/${image2}`;
-  const thirdImage = `images/project-images/${image3}`;
+  const baseUrl = "http://localhost:1337";
+
+  const imageUrl =
+    language?.projects?.[`Project${projectNumber}`]?.Images?.data[0]?.attributes
+      ?.url;
+  // console.log(baseUrl + imageUrl);
+  // console.log(projectNumber);
 
   return (
     <article className="col-6 col-12-xsmall work-item">
@@ -38,66 +37,79 @@ export function SingleProject({
         className="image fit thumb"
         style={imageStyle}
       >
-        <img src={firstImage} alt={title} className="carousel-fulls"></img>
+        <img
+          src={baseUrl + imageUrl}
+          alt="cover image"
+          className="carousel-fulls"
+        ></img>
       </button>
-      <ImageModal
-        show={modalShow}
-        onHide={() => setModalShow(false)}
-        firstImage={firstImage}
-        secondImage={secondImage}
-        thirdImage={thirdImage}
-        title={title}
-      />
+      <ImageModal show={modalShow} onHide={() => setModalShow(false)} />
       <div className="project-intro">
-        <h2>{title}</h2>
+        <h2>{language?.projects?.[`Project${projectNumber}`]?.Title}</h2>
 
         <div className="project-intro-text">
-          <p>{projectData.introText}</p>
+          <p>{language?.projects?.[`Project${projectNumber}`]?.Intro}</p>
         </div>
       </div>
       <br />
       {isVisible && (
-        <div>
-          <h4>{projectLabels.processTitle}</h4>
-          <p>{projectData.para1}</p>
-          <br />
-          <p>{projectData.para2}</p>
-          <br />
-          <p>{projectData.para3}</p>
-          <br />
-          <p>{projectData.para4}</p>
+        <div className="project-description">
+          <h4>{language?.labels?.Process}</h4>
+          <p>{language?.projects?.[`Project${projectNumber}`]?.Text}</p>
 
           <br />
         </div>
       )}
 
       <button
-        onClick={toggleVisibility}
         id="project-text-button"
         className="button"
         style={{ marginBottom: "20px" }}
+        onClick={toggleVisibility}
       >
-        {isVisible ? lang.toggleWork.hide : lang.toggleWork.show}
+        {isVisible
+          ? language?.labels?.buttonHide
+          : language?.labels?.buttonShow}
       </button>
 
       <br />
       <div className="row">
-        <p>
-          {projectLabels.technologies}
-          <span style={{ fontWeight: "bold" }}>{projectData.technologies}</span>
-        </p>
-        <p>
-          {projectLabels.site}
-          {links.site ? (
-            <a href={links.site}>{title}</a>
-          ) : (
-            <span>{projectLabels.noSite}</span>
-          )}
-        </p>
-        <p>
-          {projectLabels.repo}
-          <a href={links.repo}>GitHub {title}</a>
-        </p>
+        <div className="project-technologies">
+          <p>
+            {language?.labels?.Technologies}
+            <span style={{ fontWeight: "bold" }}>
+              {language?.projects?.[`Project${projectNumber}`]?.Technologies}
+            </span>
+          </p>
+        </div>
+        <div className="project-homepage">
+          <p>
+            {language?.labels?.Homepage}
+
+            {language?.projects?.[`Project${projectNumber}`]?.hasHomepage ===
+            false ? (
+              <span>
+                {language?.projects?.[`Project${projectNumber}`]?.Homepage}
+              </span>
+            ) : (
+              <a
+                href={language?.projects?.[`Project${projectNumber}`]?.Homepage}
+              >
+                {language?.projects?.[`Project${projectNumber}`]?.Title}
+              </a>
+            )}
+          </p>
+        </div>
+        <div className="project-repository">
+          <p>
+            {language?.labels?.Repository}
+            <a
+              href={language?.projects?.[`Project${projectNumber}`]?.Repository}
+            >
+              {language?.projects?.[`Project${projectNumber}`]?.Title}
+            </a>
+          </p>
+        </div>
       </div>
     </article>
   );
