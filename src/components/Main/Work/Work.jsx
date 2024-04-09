35a48cdcbc7c2ca3e2cca97f966fe76450fedf1f
Mark Saville <mark.saville@outlook.com>
2024-04-09 19:29:13 +0200

    Refactor Work to limit projects and pass project number as prop

--- src/components/Main/Work/Work.jsx
+++ src/components/Main/Work/Work.jsx
@@ -23,9 +23,14 @@ export const Work = () => {
         <h2>{language && language.work && language.work.Title}</h2>
         <div className="row">
           {language &&
-            Object.values(language.projects).map((workItem, index) => (
-              <SingleProject key={index} />
-            ))}
+            Object.values(language.projects)
+              .slice(0, 6) // Slice the array to get the first 6 objects
+              .map((project, index) => {
+                const projectNumber = index + 1; // Increment the index by 1 to start from 1
+                return (
+                  <SingleProject key={index} projectNumber={projectNumber} />
+                );
+              })}
         </div>
 
         <ul className="actions add-projects">
