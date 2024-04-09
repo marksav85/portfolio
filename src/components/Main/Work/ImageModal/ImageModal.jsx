45191a7a1c96b795674b8a8ba8ea348a0b76d034
Mark Saville <mark.saville@outlook.com>
2024-04-09 19:48:19 +0200

    Refactor ImageModal to use gql data and language context

--- src/components/Main/Work/ImageModal/ImageModal.jsx
+++ src/components/Main/Work/ImageModal/ImageModal.jsx
@@ -1,19 +1,35 @@
-import React from "react";
 import { Modal } from "react-bootstrap";
 import { ImageCarousel } from "../ImageCarousel/ImageCarousel";
+// Import language-related context and custom hook
+import { useLanguage } from "../../../../context/LanguageContext";
+import useLanguageContent from "../../../../hooks/useLanguageContent";
 
-export const ImageModal = ({
-  show,
-  onHide,
-  title,
-  firstImage,
-  secondImage,
-  thirdImage,
-}) => {
+export const ImageModal = ({ projectNumber, show, onHide }) => {
+  // Use language-related context and custom hook
+  const { handleLanguageChange } = useLanguage();
+  const language = useLanguageContent();
+
+  const baseUrl = "http://localhost:1337";
+  const image1Url =
+    language?.projects?.[`Project${projectNumber}`]?.Images?.data[0]?.attributes
+      ?.url;
+  const image2Url =
+    language?.projects?.[`Project${projectNumber}`]?.Images?.data[1]?.attributes
+      ?.url;
+  const image3Url =
+    language?.projects?.[`Project${projectNumber}`]?.Images?.data[2]?.attributes
+      ?.url;
+
+  const firstImage = baseUrl + image1Url;
+  const secondImage = baseUrl + image2Url;
+  const thirdImage = baseUrl + image3Url;
+
   return (
     <Modal show={show} onHide={onHide} centered keyboard={false} id="modal-box">
       <Modal.Header closeButton>
-        <Modal.Title>{title}</Modal.Title>
+        <Modal.Title>
+          {language?.projects?.[`Project${projectNumber}`]?.Title}
+        </Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <ImageCarousel
