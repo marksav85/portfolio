078576264b93eae44b8bdb6b1b4c67ed754a794c
Mark Saville <mark.saville@outlook.com>
2024-04-10 14:43:09 +0200

    Refactor MainView component to remove unused props and update imports

--- src/components/Main/MainView.jsx
+++ src/components/Main/MainView.jsx
@@ -3,13 +3,13 @@ import { Work } from "./Work/Work";
 import { Contact } from "./Contact/Contact";
 import References from "./References/References";
 
-export const MainView = ({ isEnglish }) => {
+export const MainView = () => {
   return (
     <div id="main">
-      <Profile isEnglish={isEnglish} />
-      <Work isEnglish={isEnglish} />
-      <Contact isEnglish={isEnglish} />
-      <References isEnglish={isEnglish} />
+      <Profile />
+      <Work />
+      <Contact />
+      <References />
     </div>
   );
 };
