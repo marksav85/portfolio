10b674898ad696fd3703af67068f0f8aedbbf776
Mark Saville <mark.saville@outlook.com>
2024-04-10 14:19:26 +0200

    Refactor References component to use gql data and SingleRef component

--- src/components/Main/References/References.jsx
+++ src/components/Main/References/References.jsx
@@ -1,43 +1,17 @@
-// style
-import Carousel from "react-bootstrap/Carousel";
-import * as english from "../../../languages/en.json";
-import * as german from "../../../languages/de.json";
+// Import language-related context and custom hook
+import { useLanguage } from "../../../context/LanguageContext";
+import useLanguageContent from "../../../hooks/useLanguageContent";
+import SingleReference from "./SingleReference/SingleReference";
 
-export default function References({ isEnglish }) {
-  const lang = isEnglish ? english : german;
-  const refData = lang.referenceData;
+export default function References() {
+  // Use language-related context and custom hook
+  const { handleLanguageChange } = useLanguage();
+  const language = useLanguageContent();
 
   return (
     <section id="four">
-      <h2>{lang.references.title}</h2>
-      <Carousel indicators={false}>
-        {refData.map((reference, index) => (
-          <Carousel.Item key={index} className="carousel-item">
-            <div>
-              <blockquote>&quot;{reference.quote}&quot;</blockquote>
-            </div>
-            <div>
-              <p>{reference.text}</p>
-            </div>
-            <div className="reference-image-container">
-              <img
-                className="d-block img-fluid reference-image"
-                src={`images/refs/${reference.image}`}
-                alt={`Reference ${index + 1}`}
-              />
-            </div>
-            <div className="reference-caption">
-              <a
-                href={reference.link}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {reference.linkText}
-              </a>
-            </div>
-          </Carousel.Item>
-        ))}
-      </Carousel>
+      <h2>{language?.reference?.Title}</h2>
+      <SingleReference />
     </section>
   );
 }
