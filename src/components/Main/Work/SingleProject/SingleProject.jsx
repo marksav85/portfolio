ba5138a90fcd8ff9644927cc2fd92cc68bcecc50
Mark Saville <mark.saville@outlook.com>
2024-04-10 14:49:57 +0200

    Remove unused code and update comments

--- src/components/Main/Work/SingleProject/SingleProject.jsx
+++ src/components/Main/Work/SingleProject/SingleProject.jsx
@@ -3,81 +3,100 @@ import { useState } from "react";
 import { ImageModal } from "../ImageModal/ImageModal";
 import "bootstrap/dist/css/bootstrap.min.css";
 // Import language-related context and custom hook
-import { useLanguage } from "../../../../context/LanguageContext";
+
 import useLanguageContent from "../../../../hooks/useLanguageContent";
 
 export function SingleProject({ projectNumber }) {
-  // Use language-related context and custom hook
-  const { handleLanguageChange } = useLanguage();
+  // Use language-related context and custom hook to access language content
   const language = useLanguageContent();
 
+  // Style for the project image
   const imageStyle = {
-    cursor: "pointer",
-    outline: "0px",
+    cursor: "pointer", // Set cursor style to pointer
+    outline: "0px", // Remove outline on focus
   };
+
+  // State to manage visibility of project description
   const [isVisible, setIsVisible] = useState(false);
+
+  // Function to toggle visibility of project description
   const toggleVisibility = () => {
     setIsVisible(!isVisible);
   };
 
+  // State to manage visibility of image modal
   const [modalShow, setModalShow] = useState(false);
 
+  // Base URL for image URLs
   const baseUrl = "http://localhost:1337";
 
+  // Construct URL for the project image
   const imageUrl =
     language?.projects?.[`Project${projectNumber}`]?.Images?.data[0]?.attributes
       ?.url;
-  // console.log(baseUrl + imageUrl);
-  // console.log(projectNumber);
 
   return (
     <article className="col-6 col-12-xsmall work-item">
+      {/* Button to show image modal */}
       <button
         onClick={() => setModalShow(true)}
         className="image fit thumb"
         style={imageStyle}
       >
+        {/* Display project image */}
         <img
           src={baseUrl + imageUrl}
           alt="cover image"
           className="carousel-fulls"
         ></img>
       </button>
+
+      {/* Modal for displaying project images */}
       <ImageModal
         projectNumber={projectNumber}
         show={modalShow}
         onHide={() => setModalShow(false)}
       />
+
       <div className="project-intro">
+        {/* Display project title */}
         <h2>{language?.projects?.[`Project${projectNumber}`]?.Title}</h2>
 
         <div className="project-intro-text">
+          {/* Display project introduction */}
           <p>{language?.projects?.[`Project${projectNumber}`]?.Intro}</p>
         </div>
       </div>
       <br />
+
+      {/* Display project description if isVisible is true */}
       {isVisible && (
         <div className="project-description">
           <h4>{language?.labels?.Process}</h4>
+          {/* Display project description text */}
           <p>{language?.projects?.[`Project${projectNumber}`]?.Text}</p>
-
           <br />
         </div>
       )}
 
+      {/* Button to toggle visibility of project description */}
       <button
         id="project-text-button"
         className="button"
         style={{ marginBottom: "20px" }}
         onClick={toggleVisibility}
       >
+        {/* Display button text based on visibility state */}
         {isVisible
-          ? language?.labels?.buttonHide
-          : language?.labels?.buttonShow}
+          ? language?.labels?.buttonHide // Text to show/hide project description
+          : language?.labels?.buttonShow}{" "}
       </button>
 
       <br />
+
+      {/* Display project details */}
       <div className="row">
+        {/* Display project technologies */}
         <div className="project-technologies">
           <p>
             {language?.labels?.Technologies}
@@ -86,10 +105,12 @@ export function SingleProject({ projectNumber }) {
             </span>
           </p>
         </div>
+
+        {/* Display project homepage */}
         <div className="project-homepage">
           <p>
             {language?.labels?.Homepage}
-
+            {/* Display project homepage link or text */}
             {language?.projects?.[`Project${projectNumber}`]?.hasHomepage ===
             false ? (
               <span>
@@ -104,9 +125,12 @@ export function SingleProject({ projectNumber }) {
             )}
           </p>
         </div>
+
+        {/* Display project repository */}
         <div className="project-repository">
           <p>
             {language?.labels?.Repository}
+            {/* Display project repository link */}
             <a
               href={language?.projects?.[`Project${projectNumber}`]?.Repository}
             >
