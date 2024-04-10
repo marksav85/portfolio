23d8efaeded384e2dcc76f26b1929aadb362422a
Mark Saville <mark.saville@outlook.com>
2024-04-10 14:58:45 +0200

    Update comments

--- src/components/Main/References/References.jsx
+++ src/components/Main/References/References.jsx
@@ -1,16 +1,16 @@
 // Import language-related context and custom hook
-import { useLanguage } from "../../../context/LanguageContext";
 import useLanguageContent from "../../../hooks/useLanguageContent";
 import SingleReference from "./SingleReference/SingleReference";
 
 export default function References() {
   // Use language-related context and custom hook
-  const { handleLanguageChange } = useLanguage();
   const language = useLanguageContent();
 
   return (
     <section id="four">
+      {/* Display the title from the language context */}
       <h2>{language?.reference?.Title}</h2>
+      {/* Render the SingleReference component */}
       <SingleReference />
     </section>
   );
