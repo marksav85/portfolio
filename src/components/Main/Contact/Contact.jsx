28e7d03c84597126e19743dd7ad0e0f363fccd57
Mark Saville <mark.saville@outlook.com>
2024-04-10 11:55:50 +0200

    Update Contact to use gql data and language context hook

--- src/components/Main/Contact/Contact.jsx
+++ src/components/Main/Contact/Contact.jsx
@@ -2,26 +2,38 @@ import React from "react";
 import * as english from "../../../languages/en.json";
 import * as german from "../../../languages/de.json";
 
+// Import language-related context and custom hook
+import { useLanguage } from "../../../context/LanguageContext";
+import useLanguageContent from "../../../hooks/useLanguageContent";
+
 export const Contact = ({ isEnglish }) => {
-  const lang = isEnglish ? english : german;
+  // Use language-related context and custom hook
+  const { handleLanguageChange } = useLanguage();
+  const language = useLanguageContent();
+
+  const baseUrl = "http://localhost:1337";
+  const resumeDevUrl =
+    baseUrl + language?.contact?.ResumeDeveloperLink?.data?.attributes?.url;
+  const resumeFullUrl =
+    baseUrl + language?.contact?.ResumeFullLink?.data?.attributes?.url;
 
   return (
     <>
       <section id="three">
-        <h2>{lang.contactSection.title}</h2>
-        <p>{lang.contactSection.text}</p>
+        <h2>{language?.contact?.Title}</h2>
+        <p>{language?.contact?.Subtitle}</p>
         <div className="row">
           <div className="col-4 col-12-small">
             <ul className="labeled-icons">
               <li>
                 <h3 className="icon brands fa-linkedin">
-                  <span className="label">Address</span>
+                  <span className="label">Linkedin Address</span>
                 </h3>
                 <a
                   href="https://linkedin.com/in/mark-saville"
-                  aria-label="Link to Twitter"
+                  aria-label="Link to Linedkin"
                 >
-                  <span className="label">Linkedin</span>
+                  <span className="label">{language?.contact?.Linkedin}</span>
                 </a>
               </li>
               <li>
@@ -31,7 +43,7 @@ export const Contact = ({ isEnglish }) => {
                 >
                   <span className="label">Phone</span>
                 </h3>
-                +49176 4710 7968
+                {language?.contact?.Telephone}
               </li>
               <li>
                 <h3 className="icon solid fa-envelope">
@@ -41,66 +53,39 @@ export const Contact = ({ isEnglish }) => {
                   href="mailto:mark.saville@outlook.com"
                   aria-label="Send an email"
                 >
-                  mark.saville@outlook.com
+                  {language?.contact?.Email}
                 </a>
               </li>
             </ul>
 
-            {isEnglish ? (
-              <ul className="labeled-icons">
-                <li>
-                  <h3 className="icon solid fa-download">
-                    <span className="label">Download</span>
-                  </h3>
-                  <a
-                    target="_blank"
-                    href="documents/Saville-Mark-Resume-Dev.pdf"
-                    aria-label="Download my CV"
-                  >
-                    Download Developer CV
-                  </a>
-                </li>
-                <li>
-                  <h3 className="icon solid fa-download">
-                    <span className="label">Download</span>
-                  </h3>
-                  <a
-                    target="_blank"
-                    href="documents/Saville-Mark-Resume-Full.pdf"
-                    aria-label="Download my CV"
-                  >
-                    Download Full CV
-                  </a>
-                </li>
-              </ul>
-            ) : (
-              <ul className="labeled-icons">
-                <li>
-                  <h3 className="icon solid fa-download">
-                    <span className="label">Download</span>
-                  </h3>
-                  <a
-                    target="_blank"
-                    href="documents/Saville-Mark-Lebenslauf-Entwickler.pdf"
-                    aria-label="Download my CV"
-                  >
-                    Entwickler Lebenslauf herunterladen
-                  </a>
-                </li>
-                <li>
-                  <h3 className="icon solid fa-download">
-                    <span className="label">Download</span>
-                  </h3>
-                  <a
-                    target="_blank"
-                    href="documents/Saville-Mark-Lebenslauf-vollständig.pdf"
-                    aria-label="Download my CV"
-                  >
-                    Vollständigen Lebenslauf herunterladen
-                  </a>
-                </li>
-              </ul>
-            )}
+            <ul className="labeled-icons">
+              <li>
+                <h3 className="icon solid fa-download">
+                  <span className="label">Download</span>
+                </h3>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href={resumeDevUrl}
+                  aria-label={language?.contact?.ResumeDeveloperText}
+                >
+                  {language?.contact?.ResumeDeveloperText}
+                </a>
+              </li>
+              <li>
+                <h3 className="icon solid fa-download">
+                  <span className="label">Download</span>
+                </h3>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href={resumeFullUrl}
+                  aria-label={language?.contact?.ResumeFullText}
+                >
+                  {language?.contact?.ResumeFullText}
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
       </section>
