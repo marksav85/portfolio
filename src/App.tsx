a4acdd2be5c77905597fc321f425b2d5c742892d
Mark Saville <mark.saville@outlook.com>
2024-05-20 17:27:19 +0200

    Refactor App component to remove language state and props

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
-import { useState } from "react";
-import React from "react";
 import { Header } from "./components/Header/Header";
 import { MainView } from "./components/Main/MainView";
 import { Footer } from "./components/Main/Footer/Footer";
 
 function App() {
-  const [isEnglish, setIsEnglish] = React.useState(true);
-  const handleLanguageChange = () => {
-    setIsEnglish(!isEnglish);
-  };
   return (
     <div>
-      <Header isEnglish={isEnglish} setLanguage={handleLanguageChange} />
-      <MainView isEnglish={isEnglish} />
+      <Header />
+      <MainView />
       <Footer />
     </div>
   );
