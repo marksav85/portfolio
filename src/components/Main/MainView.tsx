95dfe2037a8e61615bd0a6f424002f055e781c10
Mark Saville <mark.saville@outlook.com>
2024-05-20 17:30:08 +0200

    Refactor MainView component to use explicit return type and import React

--- src/components/Main/MainView.tsx
+++ src/components/Main/MainView.tsx
@@ -1,9 +1,11 @@
+import React from "react";
 import { Profile } from "./Profile/Profile";
 import { Work } from "./Work/Work";
 import { Contact } from "./Contact/Contact";
 import References from "./References/References";
 
-export const MainView = () => {
+// Define the functional component with an explicit return type
+export const MainView: React.FC = () => {
   return (
     <div id="main">
       <Profile />
