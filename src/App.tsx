9fcf4f590eb2de61d84c5c61d1771d59280d8e96
Mark Saville <mark.saville@outlook.com>
2024-05-22 13:15:25 +0200

    Add loading and error states

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import { Header } from "./components/Header/Header";
 import { MainView } from "./components/Main/MainView";
 import { Footer } from "./components/Main/Footer/Footer";
+import { useGetLanguages } from "./hooks/useGetLanguages";
 
 function App() {
+  const { loading, error } = useGetLanguages();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
   return (
     <div>
       <Header />
