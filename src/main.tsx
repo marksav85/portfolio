8ee83c2a00b18649b321424b3b40ab2a6059ef5b
Mark Saville <mark.saville@outlook.com>
2024-05-29 11:42:32 +0200

    Add env variables for vite

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { LanguageProvider } from "./context/LanguageContext";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { persistCache } from "apollo3-cache-persist";
 import "./assets/css/main.css";
 
 // apollo client
 const client = new ApolloClient({
-  uri: "https://strapi-ai51.onrender.com/graphql",
+  uri: import.meta.env.VITE_STRAPI_URL,
+  // uri: "import.meta.env.VITE_LOCAL_URL",
   cache: new InMemoryCache(),
+});
+
+persistCache({
+  cache: client.cache,
+  storage: window.localStorage,
 });
 
 const rootElement = document.getElementById("root");
