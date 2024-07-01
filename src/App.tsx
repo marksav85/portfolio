3c2ae519cbc2bdd071850f8d0275850b2d853596
Mark Saville <mark.saville@outlook.com>
2024-07-01 14:40:09 +0200

    Added loading state spinner and updated error state

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,41 @@ import { Header } from "./components/Header/Header";
 import { MainView } from "./components/Main/MainView";
 import { Footer } from "./components/Main/Footer/Footer";
 import { useGetLanguages } from "./hooks/useGetLanguages";
+import { useState } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
+import "./index.css";
 
 function App() {
   const { loading, error } = useGetLanguages();
+  const [color, setColor] = useState("blue");
 
   if (loading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="loader-container">
+        <ClipLoader
+          color={color}
+          size={150}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
+      </div>
+    );
   }
 
   if (error) {
-    return <div>Error: {error.message}</div>;
+    return (
+      <div className="error-container">
+        <div className="error-message">
+          <p>Oops! Something went wrong.</p>
+          <p>Bugs are currently being squashed.</p>
+          <p>Please try again later.</p>
+        </div>
+      </div>
+    );
   }
+
   return (
-    <div>
+    <div className="app-container">
       <Header />
       <MainView />
       <Footer />
